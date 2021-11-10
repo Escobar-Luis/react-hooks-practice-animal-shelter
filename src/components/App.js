3f47a25e154460affff39ab68c7f6e59b9253c13
Luis Escobar <lae311@stern.nyu.edu>
2021-11-10 14:03:06 -0500

    commit tree ddeb42b559043eebd90b11602e1dc5a32f2ca81a

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,26 @@ function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
 
+  function handleChangeType (filter) {
+    setFilters(filter)
+  }
+
+const url = (`http://localhost:3001/pets${filters.type === 'all' ? "" :  `?type=${filters.type}`}`)
+
+  function handleFindPetsClick () {
+    fetch(url)
+    .then(r=>r.json())
+    .then((pets) => setPets(pets))
+  }
+
+  //we get the pets id from the pet component through the callback function, then we update our current array by mapping through pets and returning every pet as it is unless if the pets id matches, then we construct a new object with pet and set isAdopted to true
+  function handleAdoptPet(id) {
+    const updatedPets = pets.map((pet) => {
+      return pet.id === id ? {...pet, isAdopted: true} : pet
+    })
+    setPets(updatedPets)
+    }
+
   return (
     <div className="ui container">
       <header>
@@ -15,10 +35,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onFindPetsClick={handleFindPetsClick} onChangeType={handleChangeType} />
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser onAdoptPet={handleAdoptPet} pets={pets}/>
           </div>
         </div>
       </div>
